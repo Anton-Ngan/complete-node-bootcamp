a33622089bffa6be191c61beb833440688f927be
Anton Ngan <anton.an.ngan@gmail.com>
2026-02-12 18:48:01 +1100

    feat: connect mongodb with express

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,24 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
+const mongoose = require('mongoose');
 const app = require('./app');
 
+dotenv.config({ path: './.env' });
+
 // console.log(process.env);
+
+async function connectDB() {
+  const dbString = await process.env.DB_CONNECT.replace(
+    '<PASSWORD>',
+    process.env.DB_PW,
+  );
+  try {
+    await mongoose.connect(dbString);
+    console.log('DB connection successful!');
+  } catch (err) {
+    console.log('An error occurred:', err.message);
+  }
+}
+connectDB();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
